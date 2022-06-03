eca88e893e7a09a8f14ef95012829af7564b267a
Rob Latham <robl@mcs.anl.gov>
2022-06-03 14:57:22 -0500

    debugging backend data structure

--- src/hoard.hpp
+++ src/hoard.hpp
@@ -1,10 +1,7 @@
 #include <vector>
 #include <cstdint>
 #include <cstddef>
-
-#ifdef DEBUG_HOARD
 #include <iostream>
-#endif
 
 /* just a big ol' flat array of data.  There is no paging out of excess
  * data.  no least recently used or anything like that.  Just how fast
@@ -17,6 +14,11 @@ class Hoard {
         int get(int64_t * dest, size_t count, size_t offset);
     private:
        std::vector<int64_t> m_hoard;
+       void show() {
+           for (const auto &x : m_hoard)
+               std::cout << x << " ";
+           std::cout << std::endl;
+       }
 };
 
 int Hoard::put(int64_t* src, size_t count, size_t offset)
@@ -24,16 +26,21 @@ int Hoard::put(int64_t* src, size_t count, size_t offset)
     if (m_hoard.size() < offset + count)
         m_hoard.resize( (m_hoard.size()+offset+count) * 2);
 
-    m_hoard.insert(m_hoard.begin()+offset, src, src+count);
+    // having trouble using insert() correctly concurrently...
+    //m_hoard.insert(m_hoard.begin()+offset, src, src+count);
+    for (size_t i = 0; i< count ; i++)
+        m_hoard[offset+i] = src[i];
 #ifdef DEBUG_HOARD
-    std::cout << "Hoard::put: " << src[0] << "...  " << count << " items at " << offset << "(hoard[0]: " << m_hoard[0] << std::endl;
+    std::cout << "Hoard::put: " << src[0] << "...  " << count << " items at " << offset << std::endl;;
+    show();
 #endif
     return 0;
 }
 int Hoard::get(int64_t *dest, size_t count, size_t offset)
 {
 #ifdef DEBUG_HOARD
-    std::cout << "Hoard::get: " << count << " items at " << offset << " " << m_hoard[offset] << "(hoard[0]: "<<  m_hoard[0] << std::endl;
+    std::cout << "Hoard::get: " << count << " items at " << offset << " " << m_hoard[offset] << std::endl;
+    show();
 #endif
     for (size_t i= 0; i< count; i++)
         dest[i] = m_hoard[offset+i];
